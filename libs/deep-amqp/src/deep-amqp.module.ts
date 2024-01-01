a098b2e4203621852eb71827846e5751338ba2bd
zengjc <2356924146@qq.com>
2024-01-02 00:13:31 +0800

    feat: rename

--- libs/deep-amqp/src/deep-amqp.module.ts
+++ libs/deep-amqp/src/deep-amqp.module.ts
@@ -18,7 +18,7 @@ import {
               options: { durable: false },
             },
           ],
-          uri: 'amqp://guest:guest@localhost:5672',
+          uri: 'amqp://guest:guest@127.0.0.1:5672',
           connectionInitOptions: { wait: false },
           enableDirectReplyTo: false,
           enableControllerDiscovery: true,
