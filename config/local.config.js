da5d391f464118a0cfaafb2005f15eb921ec0a87
zengjc <2356924146@qq.com>
2024-01-07 15:03:41 +0800

    feat: global config

---
+++ config/local.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  mysql: {
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '123',
+    database: 'coderhub',
+    retryDelay: 500,
+    retryAttempts: 5,
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+  redis: {
+    socket: {
+      host: '127.0.0.1',
+      port: 6379,
+    },
+  },
+  rabbitmq: {
+    exchanges: [
+      {
+        name: 'deep_exchange',
+        type: 'direct',
+        options: { durable: false },
+      },
+    ],
+    uri: 'amqp://guest:guest@127.0.0.1:5672',
+    connectionInitOptions: { wait: false },
+    enableDirectReplyTo: false,
+    enableControllerDiscovery: true,
+    prefetchCount: 300,
+    defaultSubscribeErrorBehavior: 'ACK',
+  },
+};
