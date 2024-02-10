5b8b36496c094d7a638830d4021530a9c3927bd5
zengjc <2356924146@qq.com>
2024-02-10 12:34:14 +0800

    fix: 缺少依赖

--- apps/deep-backend/src/app.module.ts
+++ apps/deep-backend/src/app.module.ts
@@ -11,9 +11,18 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { CheckResourceOwnershipGuard } from './common/guard/checkResourceOwnership.guard';
+import { DeepDbModule } from '@app/deep-db';
+import { CacheModule } from '@app/deep-cache';
+import { SecretKeyModule } from '@app/common/secretKey/secretKey.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [
+    DeepDbModule,
+    CacheModule,
+    DatabaseModule,
+    SecretKeyModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
