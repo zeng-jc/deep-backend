78dfe7eb32c1f411b8058b75fb6793403392d68d
zengjc <2356924146@qq.com>
2024-03-12 00:37:38 +0800

    fix: git filename bug

---
+++ libs/common/src/exceptionFilter/httpException.filter.ts
@@ -0,0 +1,30 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { DeepHttpException } from './deepHttpException.filter';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    if (exception instanceof DeepHttpException) {
+      response.status(status).json({
+        path: request.url,
+        timestamp: new Date().toISOString(),
+        message: exception.getErrMsg(),
+        code: exception.getErrCode(),
+      });
+      return;
+    }
+
+    response.status(status).json({
+      path: request.url,
+      timestamp: new Date().toISOString(),
+      message: exception.message,
+      code: status,
+    });
+  }
+}
