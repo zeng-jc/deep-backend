2f13f333df9c73ad08213ca09394facd3cf8cfc1
zengjc <2356924146@qq.com>
2024-04-21 11:23:42 +0800

    feat: update code

--- apps/deep-backend/src/article-comment/dto/create-article-comment.dto.ts
+++ apps/deep-backend/src/article-comment/dto/create-article-comment.dto.ts
@@ -1,8 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 export class CreateArticleCommentDto {
   @IsNotEmpty()
-  userId!: string;
-  @IsNotEmpty()
   articleId!: string;
   @IsNotEmpty()
   content!: string;
